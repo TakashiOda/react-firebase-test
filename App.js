afc01cc4e047b9518416e0373c0a63dabfc363e8
Takashi Oda <wiwi5223@gmail.com>
2020-05-30 06:20:35 +0900

    リストアイテムview作成

--- App.js
+++ App.js
@@ -26,13 +26,43 @@ if (!firebase.apps.length) {
 export default class App extends React.Component {
   render() {
     return (
-      <Container style={styles.container}>
-        <View>
-          <View>aa</View>
-          <Text>Memoapp</Text>
-          <BodyText>hihihihihih</BodyText>
+      <View style={styles.container}>
+        <View style={styles.appbar}>
+          <View>
+            <Text style={styles.appbarTitle}>Memoapp</Text>
+          </View>
         </View>
-      </Container>
+
+        <View style={styles.memoList}>
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2020/05/29</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2020/05/29</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2020/05/29</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2020/05/29</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2020/05/29</Text>
+          </View>
+        </View>
+        <View style={styles.memoAddButton}>
+          <Text style={styles.memoAddButtonTitle}>+</Text>
+        </View>
+      </View>
     );
   }
 }
@@ -40,8 +70,68 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFDF6',
     justifyContent: 'center',
     alignItems: 'center',
+    paddingTop: 78,
+  },
+  memoAddButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#e31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  memoAddButtonTitle: {
+    fontSize: 32,
+    color: '#eee',
+  },
+  memoList: {
+    // backgroundColor: "#eee",
+    width: '100%',
+    flex: 1,
+  },
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+    backgroundColor: '#fff',
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2',
+  },
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 78,
+    paddingTop: 30,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    zIndex: 10,
+  },
+  appbarTitle: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
--- src/elements/BodyText.js
+++ src/elements/BodyText.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 class BodyText extends React.Component {
-    render() {
-        return (
-            <View>
-                <Text style={styles.text}>
-                    {this.props.children}
-                </Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View>
+        <Text style={styles.text}>
+          { this.props.children }
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    text: {
-        color: '#DDD',
-        backgroundColor: '#eee'
-    }
+  text: {
+    color: '#DDD',
+    backgroundColor: '#eee',
+  },
 });
 
 export default BodyText;
