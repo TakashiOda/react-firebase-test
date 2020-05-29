8db1f4c8adbef39225976c27c2141a08686add16
Takashi Oda <wiwi5223@gmail.com>
2020-05-29 19:03:43 +0900

    second

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Container style={styles.container}>
-        <Text>bbb</Text>
+        <Text>bbbcccddd</Text>
       </Container>
     );
   }
