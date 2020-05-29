d9e04c3e617c4e5b595413595631df9cb9f52c27
Takashi Oda <wiwi5223@gmail.com>
2020-05-29 20:28:50 +0900

    .eslintrcにコンマ修正

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Expo from 'expo';
-import { StyleSheet, Text, View, StatusBar, TextInput } from 'react-native';
-import { Container, Content, Header, Form, Input, Button, Label, Icon, List, ListItem } from 'native-base';
+import { StyleSheet, Text, View } from 'react-native';
+import {
+  Container, Content, Header, Form, Input, Button, Label, Icon, List, ListItem,
+} from 'native-base';
 // import ListView from "deprecated-react-native-swipeable-listview";
 import * as firebase from 'firebase';
 // import * as Facebook from 'expo-facebook';
@@ -9,18 +11,17 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyC-eM-1EI_QNNky9J17y_rgjzX_O82nyhE",
-  authDomain: "react-firebase-a9be0.firebaseapp.com",
-  databaseURL: "https://react-firebase-a9be0.firebaseio.com",
-  projectId: "react-firebase-a9be0",
-  storageBucket: "react-firebase-a9be0.appspot.com",
+  apiKey: 'AIzaSyC-eM-1EI_QNNky9J17y_rgjzX_O82nyhE',
+  authDomain: 'react-firebase-a9be0.firebaseapp.com',
+  databaseURL: 'https://react-firebase-a9be0.firebaseio.com',
+  projectId: 'react-firebase-a9be0',
+  storageBucket: 'react-firebase-a9be0.appspot.com',
 };
 
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
-
 
 
 export default class App extends React.Component {
@@ -38,6 +39,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
 });
