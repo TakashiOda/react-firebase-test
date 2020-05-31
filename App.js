779002489c796eed35e9bf0fa8094ff99e3a9a88
Takashi Oda <wiwi5223@gmail.com>
2020-05-31 23:10:38 +0900

    ログイン画面 メンバー登録画面追加

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ import { StyleSheet, View } from 'react-native';
 import * as firebase from 'firebase';
 // import * as Facebook from 'expo-facebook';
 import Appbar from './src/components/Appbar';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+import SignupScreen from './src/screens/SignupScreen';
+// import LoginScreen from './src/screens/LoginScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 
@@ -30,7 +32,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoEditScreen />
+        <SignupScreen />
       </View>
     );
   }
